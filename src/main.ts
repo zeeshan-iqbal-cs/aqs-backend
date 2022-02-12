18ee2aee054367e16990bc0aa1061927c63b5b00
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2022-02-12 05:42:30 +0500

    mongoose and swagger are added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const packageJson = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('AQS (Air Quality Sensor)')
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
