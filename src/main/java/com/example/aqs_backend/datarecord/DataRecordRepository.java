810dd54fb54e64ca0ae2b3f750f396706c671660
Zeeshan Iqbal <zeeshan.iqbal.cs@gmail.com>
2022-02-12 05:23:19 +0500

    SWITCHED: To nestjs

--- src/main/java/com/example/aqs_backend/datarecord/DataRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.aqs_backend.datarecord;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataRecordRepository extends JpaRepository<DataRecord,Long> {
-}
--- src/main/java/com/example/aqs_backend/partner/PartnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aqs_backend.partner;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartnerRepository extends JpaRepository<Partner,Long> {
-    public Partner findByEmail(String email);
-}
